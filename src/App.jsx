492a8adce0bf3e0af7b5ac20b21b2b12a9a107e1
YamaCode <bradt.chancan2@gmail.com>
2025-05-26 01:27:29 -0500

    Primera colaboracion contigo ♥

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    
-  <p className="text-red-950 bg-amber-200">HOLA MUNDO</p>
-      
-    </>
-  )
+    <div className="flex flex-col items-center justify-center gap-5">
+      <p className="text-red-950 bg-amber-200">HOLA MUNDO</p>
+      <div className="flex flex-col items-center justify-center bg-slate-200 shadow-2xl p-5 rounded-2xl">
+        <p className="text-xl text-red-500 font-bold">La quiero mucho mi princesa</p>
+        <p>♥️♥️♥️♥️</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
