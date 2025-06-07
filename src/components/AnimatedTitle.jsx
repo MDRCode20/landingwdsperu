1a34f8572a462f9a27472e1b22a84a301c82f7bb
MDRCode20 <milagrosdayanarosalesapaza@gmail.com>
2025-06-07 00:09:46 -0500

    ac

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,15 @@
+
+const title = "Impulsa tu marca con diseño profesional";
+
+export default function AnimatedTitle() {
+  return (
+    <h2 className="text-5xl font-extrabold mb-6 custom-animated-title">
+      {title.split("").map((char, i) => (
+        <span key={i} style={{ animationDelay: `${(i + 1) * 0.1}s` }}>
+          {char === " " ? "\u00A0" : char}
+        </span>
+      ))}
+    </h2>
+  );
+}
+
